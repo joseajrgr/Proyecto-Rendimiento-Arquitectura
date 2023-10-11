a389b52cc4b781d161f6d58e8f3a27f372fb1ff2
100472166 <100472166@alumnos.uc3m.es>
2023-10-11 14:38:03 +0200

    calngtidy

--- fluid/fluid.cpp
+++ fluid/fluid.cpp
@@ -6,17 +6,16 @@
 
 struct Particle {
     float px, py, pz;
-    float hvx, hvy, hvz;
-    float vx, vy, vz;
+    //float hvx, hvy, hvz;
+    //float vx, vy, vz;
 };
 int main(int argc, char* argv[]) {
     if (argc != 4) {
         std::cerr << "Error: Invalid number of arguments. Usage: " << argv[0] << " <nts> <inputfile> <outputfile>\n";
         return -1;
     }
-    int iteraciones;
+    const int iteraciones = std::stoi(argv[1]);
     try {
-        iteraciones = std::stoi(argv[1]);
         if (iteraciones<0){
             std::cerr << "Error: Invalid number of time steps.\n";
             return -2;
@@ -26,8 +25,8 @@ int main(int argc, char* argv[]) {
         return -1;
     }
 
-    std::string archivoEntrada = argv[2];
-    std::string archivoSalida = argv[3];
+    const std::string archivoEntrada = argv[2];
+    const std::string archivoSalida = argv[3];
 
     // Leer el archivo de entrada
     std::ifstream input(archivoEntrada, std::ios::binary);
@@ -36,52 +35,58 @@ int main(int argc, char* argv[]) {
         return -3;
     }
 
-    float ppm;
-    int np;
-    input.read(reinterpret_cast<char*>(&ppm), sizeof(float));
-    input.read(reinterpret_cast<char*>(&np), sizeof(int));
+    float particlespermetro =0.0;
+    int numberparticles = 0;
+    input.read(reinterpret_cast<char*>(&particlespermetro), sizeof(float));
+    input.read(reinterpret_cast<char*>(&numberparticles), sizeof(int));
 
-    std::vector<Particle> particles(np);
-    input.read(reinterpret_cast<char*>(particles.data()), np * sizeof(Particle));
+    std::vector<Particle> particles(numberparticles);
+    input.read(reinterpret_cast<char*>(particles.data()), numberparticles * sizeof(Particle));
     input.close();
 
 
-    // Verificar si el número de partículas leídas coincide con np OPCIONAL
-    int numParticulasLeidas = particles.size();
-    if (numParticulasLeidas != np) {
-        std::cerr << "Error: El número de partículas leídas (" << numParticulasLeidas << ") no coincide con np (" << np << ").\n";
+    // Verificar si el número de partículas leídas coincide con numberparticles OPCIONAL
+    const int numParticulasLeidas = particles.size();
+    if (numParticulasLeidas != numberparticles) {
+        std::cerr << "Error: El número de partículas leídas (" << numParticulasLeidas << ") no coincide con numberparticles (" << numberparticles << ").\n";
         return -5;
     }
 
     // Calcular otros valores requeridos
-    float smoothingLength = 1.695 / ppm;
-    float particleMass = std::pow(10, 3) / std::pow(ppm, 3);
+    const double smoothingLength = 1.695 / particlespermetro;
+    const double particleMass = std::pow(10, 3) / std::pow(particlespermetro, 3);
 
     // Calcular el tamaño de la malla (grid size) y el número de bloques
-    float xmax = 0.065, ymax = 0.1, zmax = 0.065;
-    float xmin = -0.065, ymin = -0.08, zmin = -0.065;
-    int nx = static_cast<int>((xmax - xmin) / smoothingLength);
-    int ny = static_cast<int>((ymax - ymin) / smoothingLength);
-    int nz = static_cast<int>((zmax - zmin) / smoothingLength);
-    int numBlocks = nx * ny * nz;
-    float sx = (xmax - xmin) / nx;
-    float sy = (ymax - ymin) / ny;
-    float sz = (zmax - zmin) / nz;
+    const float xmax = 0.065;
+    const float ymax = 0.1;
+    const float zmax = 0.065;
+    const float xmin = -0.065;
+    const float ymin = -0.08 ;
+    const float zmin = -0.065;
+    const float numberblocksx = std::floor((xmax - xmin) / smoothingLength);
+    const float numberblocksy = std::floor((ymax - ymin) / smoothingLength);
+    const float numberblocksz = std::floor((zmax - zmin) / smoothingLength);
+
+    const float numBlocks = numberblocksx * numberblocksy * numberblocksz;
+    const float meshx = (xmax - xmin) / numberblocksx;
+    const float meshy = (ymax - ymin) / numberblocksy;
+
+    const float meshz = (zmax - zmin) / numberblocksz;
 
     // Mostrar los valores por pantalla en el formato requerido
-    std::cout << "Number of particles: " << np << std::endl;
-    std::cout << "Particles per meter: " << ppm << std::endl;
-    std::cout << "Smoothing length: " << smoothingLength << std::endl;
-    std::cout << "Particle mass: " << particleMass << std::endl;
-    std::cout << "Grid size: " << nx << " x " << ny << " x " << nz << std::endl;
-    std::cout << "Number of blocks: " << numBlocks << std::endl;
-    std::cout << "Block size: " << sx << " x " << sy << " x " << sz << std::endl;
+    std::cout << "Number of particles: " << numberparticles << "\n";
+    std::cout << "Particles per meter: " << particlespermetro << "\n";
+    std::cout << "Smoothing length: " << smoothingLength << "\n";
+    std::cout << "Particle mass: " << particleMass << "\n";
+    std::cout << "Grid size: " << numberblocksx << " x " << numberblocksy << " x " << numberblocksz << "\n";
+    std::cout << "Number of blocks: " << numBlocks << "\n";
+    std::cout << "Block size: " << meshx << " x " << meshy << " x " << meshz << "\n";
 
     // Realizar la simulación
     for (int iter = 0; iter < iteraciones; ++iter) {
         std::cout << "Iteración " << iter + 1 << ":\n";
-        for (int i = 0; i < np; ++i) {
-            Particle& particula = particles[i];
+        for (int i = 0; i < numberparticles; ++i) {
+            const Particle& particula = particles[i];
             // Aquí se puede hacer la simulación para la partícula actual
             std::cout << "Partícula " << i + 1 << ": px=" << particula.px << " py=" << particula.py << " pz=" << particula.pz << "\n";
 
@@ -96,9 +101,11 @@ int main(int argc, char* argv[]) {
         return -4;
     }
 
-    output.write(reinterpret_cast<char*>(&ppm), sizeof(float));
-    output.write(reinterpret_cast<char*>(&np), sizeof(int));
-    output.write(reinterpret_cast<char*>(particles.data()), np * sizeof(Particle));
+    output.write(reinterpret_cast<const char*>(&particlespermetro), sizeof(float));
+    output.write(reinterpret_cast<const char*>(&numberparticles), sizeof(int));
+    output.write(reinterpret_cast<const char*>(particles.data()), numberparticles * sizeof(Particle));
+
+
     output.close();
     std::cout << "Simulación completada. Estado final del fluido guardado en: " << archivoSalida << "\n";
 
