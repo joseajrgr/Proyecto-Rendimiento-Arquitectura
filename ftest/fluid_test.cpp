e922bf6d4a79aba7f1ab780a3f26cdf8f0cb7c08
MarioRodriguezuc3m <100472194@alumnos.uc3m.es>
2023-11-17 18:50:03 +0100

    Configuracion ftest

---
+++ ftest/fluid_test.cpp
@@ -0,0 +1,21 @@
+#include <gtest/gtest.h>
+#include "sim/grid.cpp"
+
+TEST(Fluid_Tests, El_Nano777) {
+    // Arrange
+    int result;
+    // Act
+    result = 33;
+    // Assert
+    ASSERT_EQ(result, 33);
+}
+
+TEST(Fluid_Tests, Not_El_Nano777) {
+    // Arrange
+    int result;
+    // Act
+    result = 32;
+
+    // Assert
+    ASSERT_NE(result, 33);
+}
