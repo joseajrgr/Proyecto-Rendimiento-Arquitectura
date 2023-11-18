33c2d7f134a7fd0b5753582004c7e7d7bc3b5a6b
100472166 <10472166@alumnos.uc3m.es>
2023-11-18 09:55:47 +0100

    correcion funciones que devulven numberblokccs

--- sim/grid.hpp
+++ sim/grid.hpp
@@ -11,34 +11,35 @@ struct Fluid {
 };
 
 class Grid {
-    public:
-        [[nodiscard]] double getNumberblocksx() const;
-        [[nodiscard]] double getNumberblocksy() const;
-        [[nodiscard]] double getNumberblocksz() const;
-        [[nodiscard]] double getNumBlocks() const;
-        [[nodiscard]] double getMeshx() const;
-        [[nodiscard]] double getMeshy() const;
-        [[nodiscard]] double getMeshz() const;
+public:
+    [[nodiscard]] inline double getNumberblocksx() const { return numberblocksx; }
+    [[nodiscard]] inline double getNumberblocksy() const { return numberblocksy; }
+    [[nodiscard]] inline double getNumberblocksz() const { return numberblocksz; }
+    [[nodiscard]] inline double getNumBlocks() const { return numBlocks; }
+    [[nodiscard]] inline double getMeshx() const { return meshx; }
+    [[nodiscard]] inline double getMeshy() const { return meshy; }
+    [[nodiscard]] inline double getMeshz() const { return meshz; }
 
-        Grid(const Punto& bmin, const Punto& bmax);
-        void dividirEnBloques(double smoothingLength);
-        void reposicionarParticulasFluid(Fluid &fluid, std::vector<Block> &blocks) const;
-        void reposicionarParticulasBloque(std::vector<Block> &blocks) const;
-        static std::vector<Particle>::iterator comprobarBloque(Block &block, Particle &particula, Block &newBlock, std::vector<Particle>::iterator iterador);
-        [[nodiscard]] const std::vector<Block>& getBlocks() const;
+    Grid(const Punto& bmin, const Punto& bmax);
+    void dividirEnBloques(double smoothingLength);
+    void reposicionarParticulasFluid(Fluid &fluid, std::vector<Block> &blocks) const;
+    void reposicionarParticulasBloque(std::vector<Block> &blocks) const;
+    static std::vector<Particle>::iterator comprobarBloque(Block &block, Particle &particula, Block &newBlock, std::vector<Particle>::iterator iterador);
+    [[nodiscard]] inline const std::vector<Block>& getBlocks() const { return blocks; }
 
-    private:
-        double numberblocksx;
-        double numberblocksy;
-        double numberblocksz;
-        double numBlocks;
-        double meshx;
-        double meshy;
-        double meshz;
-        Punto bmin; // Limite inferior del recinto
-        Punto bmax; // Limite superior del recinto
-        std::vector<Block> blocks;
+private:
+    double numberblocksx;
+    double numberblocksy;
+    double numberblocksz;
+    double numBlocks;
+    double meshx;
+    double meshy;
+    double meshz;
+    Punto bmin; // Limite inferior del recinto
+    Punto bmax; // Limite superior del recinto
+    std::vector<Block> blocks;
 };
+
 
 
 #endif //PROYECTO_RENDIMIENTO_ARQUITECTURA_GRID_HPP
